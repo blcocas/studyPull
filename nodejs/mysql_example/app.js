ceedef829802272daef8fbcff11345918d34b7d6
Hong Jae Lee <blcocas@naver.com>
2018-08-16 12:55:58 +0900

    mysql연동 -성공(insert,update,query,delete)

--- nodejs/mysql_example/app.js
+++ nodejs/mysql_example/app.js
@@ -15,21 +15,67 @@
 // app.locals.pretty = true; //html소스 이쁘게
 // //nodejs-mysql
 const mysql = require('mysql');
-var connection = mysql.createConnection({
+var conn = mysql.createConnection({
   host : 'localhost',
   user : 'root',
   password : 'ghdwo966',
   database : 'o2'
 });
-connection.connect();
+conn.connect(function(err){
+  if (err) throw err;
+});
+
+
+
+// var sql = 'select * from student';
+// conn.query(sql, function(err, rows, fields){
+//   if (err) throw err;
+//   else{
+//     for(var i=0;i<rows.length;i++){
+//       console.log(rows[i].name);
+//     }
+//   }
+// })
+
+// var sql = 'insert into student values(201354243,"LEE YONGJAE",4)';
+// conn.query(sql,function(err,rows,fields){
+//   if(err)
+//     console.log(err);
+//   else{
+//     console.log(rows.insertId);
+//   }
+// })
+
+// var sql = 'insert into student values(?,?,?)';
+// var params = [201224322,'PARK SENGHYUN',2];
+// conn.query(sql,params,function(err,rows,fields){
+//   if(err)
+//     console.log(err);
+//   else{
+//     console.log(rows);
+//     console.log(rows.insertId);
+//   }
+// })
+var sql = 'update student set name=?, class=? where id=?';
+var params = ['SUPERHONGJAE',99,201221027];
+conn.query(sql,params,function(err,rows,fields){
+  if(err)
+    console.log(err);
+  else{
+    console.log(rows);
+  }
+})
+
+
+conn.end();
 
 //get & post-----------------------------------
-// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+// conn.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
 //   if (error) throw error;
 //   console.log('The solution is: ', results[0].solution);
 // });
 //
-// connection.end();
+// conn.end();
 //
 // //server-----------------------------------
 // app.listen(3000, function(){
