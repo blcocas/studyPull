2f56b2a9a5b34adf92352ee48027ae261b8261fa
Hong Jae Lee <blcocas@naver.com>
2018-08-22 11:56:05 +0900

    mysql webapp실습2

--- nodejs/webapp_mysql2/app.js
+++ nodejs/webapp_mysql2/app.js
@@ -27,103 +27,86 @@ conn.connect(function(err){
 });
 
 //get & post-----------------------------------
-
 app.get('/topic/add',function(req,res){
   var sql = 'SELECT id,title FROM topic';
   conn.query(sql,function(err,rows){
     if(err) throw err;
-    res.render('add',{list : rows});
+    res.render('add', {list : rows});
   })
 })
-
-app.get('/topic/:id/edit',function(req,res){
-  var sql = 'SELECT id,title FROM topic';
-  conn.query(sql,function(err,rows1){
-    var id = req.params.id;
-    if(err) throw err;
-    if(id){
-      var sql = 'SELECT * FROM topic WHERE id=?'
-      conn.query(sql,id,function(err,rows2){
-        if(err) throw err;
-        res.render('edit',{list : rows1, topic:rows2[0]})
-      })
-    }
-  })
-})
-
-app.get(['/topic', '/topic/:id'],function(req,res){
-  var sql = 'SELECT id,title FROM topic';
-  conn.query(sql,function(err,rows1){
-    var id = req.params.id;
-    if(err) throw err;
-    if(id){
-      var sql = 'SELECT * FROM topic WHERE id=?'
-      conn.query(sql,id,function(err,rows2){
-        if(err) throw err;
-        res.render('topic',{list : rows1, topic:rows2[0]})
-      })
-    }
-    else res.render('topic',{list : rows1});
-  })
-
-})
-
 
 app.post('/topic/add',function(req,res){
   var title = req.body.title;
-  var desc = req.body.description;
+  var desc = req.body.desc;
   var author = req.body.author;
-  var sql = 'INSERT INTO topic (title, description, author) VALUES (?,?,?)'
+  var sql = 'INSERT INTO topic(title,description,author) VALUES (?,?,?)';
   var params = [title,desc,author];
   conn.query(sql,params,function(err,rows){
     if(err) throw err;
     res.redirect('/topic/'+rows.insertId);
   })
+
+})
+
+app.get('/topic/:id/edit',function(req,res){
+  var id = req.params.id;
+  var sql = 'SELECT id,title FROM topic';
+  conn.query(sql,function(err,rows1){
+    if(err) throw err;
+    if(id){
+      var sql = 'SELECT * FROM topic WHERE id = ?'
+      conn.query(sql,id,function(err,rows2){
+        if(err) throw err;
+        res.render('edit',{list : rows1, topic : rows2[0]});
+      })
+    }
+  })
 })
 
 app.post('/topic/:id/edit',function(req,res){
-  var id = req.params.id
-  var title = req.body.title;
-  var desc = req.body.description;
-  var author = req.body.author;
-  var sql = 'UPDATE topic SET title=?, description=?, author=? WHERE id=?'
-  var params = [title,desc,author,id];
+  var sql = 'UPDATE topic SET title = ?, description = ?, author = ? WHERE id = ?'
+  var params = [req.body.title,req.body.desc,req.body.author,req.params.id];
   conn.query(sql,params,function(err,rows){
     if(err) throw err;
-    res.redirect('/topic/'+id);
+    res.redirect('/topic/'+req.params.id);
   })
 })
 
 app.get('/topic/:id/delete',function(req,res){
+  var id = req.params.id;
+  var sql = 'SELECT id,title FROM topic';
+  conn.query(sql,function(err,rows){
+    if(err) throw err;
+    res.render('delete', {list : rows, id: id });
+  })
+})
+
+app.post('/topic/:id/delete',function(req,res){
+  var sql = 'DELETE FROM topic where id=?'
+  conn.query(sql,req.params.id,function(err,rows){
+    if(err) throw err;
+    res.redirect('/topic');
+  })
+})
+
+app.get(['/topic','/topic/:id'],function(req,res){
+  var id = req.params.id;
   var sql = 'SELECT id,title FROM topic';
   conn.query(sql,function(err,rows1){
-    var id = req.params.id;
     if(err) throw err;
     if(id){
-      var sql = 'SELECT * FROM topic WHERE id=?'
+      var sql = 'SELECT * FROM topic WHERE id = ?'
       conn.query(sql,id,function(err,rows2){
         if(err) throw err;
-        res.render('delete',{list : rows1, topic:rows2[0]})
+        res.render('topic',{list : rows1, topic : rows2[0]});
       })
     }
-    else res.render('delete',{list : rows1});
+    else
+      res.render('topic',{list : rows1});
   })
+})
 
-  // var sql = 'DELETE FROM topic WHERE id=?'
-  // var id=req.params.id;
-  // conn.query(sql,id,function(err,rows){
-  //   if(err) throw err;
-  //   res.redirect('/topic/'+id);
-  // })
-})
-app.post('/topic/:id/delete',function(req,res){
-  var sql = 'DELETE FROM topic WHERE id=?'
-  var id=req.params.id;
-  conn.query(sql,id,function(err,rows){
-    if(err) throw err;
-    res.redirect('/topic/'+id);
-  })
-})
+
 
 
 
